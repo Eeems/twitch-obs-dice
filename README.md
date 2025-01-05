0651dcc1c653b34cb6dd228b1feca32e07ef4efb
Nathaniel van Diepen <eeems@eeems.email>
2025-01-04 17:49:55 -0700

    Update to yml and allow scripts

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-Configuration
-=============
+Installation
+============
 
 - Go to https://dev.twitch.tv/console and create a new application.
 - Set the OAuth Redirection URL to http://localhost:17563
@@ -12,3 +12,59 @@ Configuration
 - Set the Scene to the name of the scene you want the dice to be added to
 - Set the OBS connection information to the connections settings found in OBS under Tools > WebSocket Server Settings > Show Connect Info
 - Run the bot
+
+Configuration
+=============
+
+Display
+-------
+
+Color: Colour of the dice.
+Label: Colour of the numbers on the dice.
+ChromaKey: Colour used for the background that is filtered out. This can affect the colour of the dice, so you may need to change this to get your dice to display the way you want them to.
+
+
+Twitch
+------
+
+ClientId: Client Id found in https://dev.twitch.tv/console/apps
+ClientSecret: Client secret found in https://dev.twitch.tv/console/apps
+Channel: Channel name to listen to chat for
+
+OBS
+---
+
+Scene: Scene in OBS to inject the dice roll browser sources. Add this scene as an item on your active scene to control where the dice are displayed.
+Host: Host/IP for OBS websocket server
+Port: Port for OBS websocket server
+Password: Password for OBS websocket server
+
+Commands
+--------
+
+You can add another named commands that the bot will register. The key for the command will be the twitch command that is registered to trigger this command. For example a key of `roll:` will mean that it executes when a user says `!roll`
+
+Dice: Dice specification to roll. e.g. 2d20+1d10
+Message: Message to reply to the user with in twitch chat. `{user}` will be replaced with the username and `{result}` will be replaced with the dice total that was rolled.
+DisplayTime: How long to display the dice roll for in seconds. Defaults to 5 seconds.
+Script: Python script to run for this command.
+
+
+Python Scripts
+--------------
+
+Python script have the following global variables available:
+
+- `chat`: TwitchAPI Chat instance
+- `config`: Configuration dictionary generated from the contents in `config.yml`
+- `obs`: OBS websocket instance
+- `roll_dice`: Method to display a dice roll and display the results
+- `twitch`: TwitchAPI instance
+
+Python scripts have the following local variables available:
+
+- `message`: TwitchAPI ChatCommand instance
+- `command`: Specific command configuration for this command
+- `dice`: Dice spec
+- `result`: Total of all the dice rolled
+- `results`: Breakdown of the different dice results. This can be cross referenced with `dice` to see which dice is of what type.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
+import ast
 import asyncio
+import os
 import tomllib
 
 import obsws_python as obs
@@ -17,47 +18,99 @@ from twitchAPI.type import AuthScope
 from twitchAPI.type import ChatEvent
 
 
-def roll_command(command):
+async def roll_dice(
+    name, spec: str, scene: str, displayTime: int
+) -> tuple[list[str], int, list[list[int]]]:
+    results = []
+    result = 0
+    dice = [x.split("d") for x in spec.split("+")]
+    for die in dice:
+        for i in range(0, int(die[0])):
+            if die[1] in ("4", "6", "8", "12", "20"):
+                value = randint(1, int(die[1]))
+                results.append(str(value))
+                result += value
+
+            elif die[1] == "10":
+                value = randint(0, 9)
+                results.append(str(value))
+                result += value
+
+            elif die[1] == "100":
+                value = randint(0, 9)
+                results.append(str(value))
+                result += value * 10
+
+    scene_item_id = set_dice(
+        f"Dice_!{name}",
+        command["Dice"],
+        results,
+    )
+    cl.set_scene_item_enabled(scene, scene_item_id, True)
+    await asyncio.sleep(displayTime)
+    cl.set_scene_item_enabled(scene, scene_item_id, False)
+
+    return dice, result, results
+
+
+def roll_command(name, command):
+    if "Script" in command and command["Script"]:
+        code = compile(
+            command["Script"],
+            "<script>",
+            "exec",
+            flags=ast.PyCF_ALLOW_TOP_LEVEL_AWAIT,
+        )
+
     condition = asyncio.Condition()
 
     async def func(cmd: ChatCommand):
         await condition.acquire()
         try:
-            dice = [x.split("d") for x in command["Dice"].split("+")]
-            results = []
-            result = 0
-            for die in dice:
-                for i in range(0, int(die[0])):
-                    if die[1] in ("4", "6", "8", "12", "20"):
-                        value = randint(1, int(die[1]))
-                        results.append(str(value))
-                        result += value
+            if "Dice" in command and command["Dice"]:
+                dice, result, results = await roll_dice(
+                    name,
+                    command["Dice"],
+                    config["OBS"]["Scene"],
+                    command.get("DisplayTime", 5),
+                )
 
-                    elif die[1] == "10":
-                        value = randint(0, 9)
-                        results.append(str(value))
-                        result += value
+            else:
+                results = []
+                result = 0
+                dice = []
 
-                    elif die[1] == "100":
-                        value = randint(0, 9)
-                        results.append(str(value))
-                        result += value * 10
-
-            scene_item_id = set_dice(
-                f'Dice_!{command["Name"]}',
-                command["Dice"],
-                results,
-            )
-            scene = config["OBS"]["Scene"]
-            cl.set_scene_item_enabled(scene, scene_item_id, True)
-            await asyncio.sleep(command["DisplayTime"])
-            cl.set_scene_item_enabled(scene, scene_item_id, False)
             if "Message" in command and command["Message"]:
                 await cmd.reply(
                     command["Message"]
                     .replace("{user}", f"{cmd.user.name}")
                     .replace("{result}", str(result))
                 )
+
+            if "Script" in command and command["Script"]:
+                try:
+                    coroutine: Awaitable | None = eval(
+                        code,
+                        {
+                            "chat": cmd.chat,
+                            "config": config,
+                            "obs": cl,
+                            "roll_dice": roll_dice,
+                            "twitch": cmd.chat.twitch,
+                        },
+                        {
+                            "message": cmd,
+                            "command": command,
+                            "dice": dice,
+                            "result": result,
+                            "results": results,
+                        },
+                    )
+                    if coroutine is not None:
+                        await coroutine
+
+                except Exception as e:
+                    print(e)
 
         finally:
             condition.release()
@@ -72,9 +125,9 @@ def get_dimensions():
 
 def set_dice(name, dice, values):
     itemId = ensure_dice_source(name)
-    color = config["General"]["Color"]
-    label = config["General"]["Label"]
-    chromaKey = config["General"]["ChromaKey"]
+    color = config["Display"]["Color"]
+    label = config["Display"]["Label"]
+    chromaKey = config["Display"]["ChromaKey"]
     width, height = get_dimensions()
     cl.set_input_settings(
         name,
@@ -157,7 +210,7 @@ def ensure_dice_source(name):
         name,
         "Chroma Key",
         "chroma_key_filter_v2",
-        {"key_color_type": "custom", "color": config["General"]["ChromaKey"]},
+        {"key_color_type": "custom", "color": config["Display"]["ChromaKey"]},
     )
     return itemId
 
@@ -176,8 +229,8 @@ async def run():
     await twitch.set_user_authentication(token, scope, refresh_token)
     chat = await Chat(twitch)
     chat.register_event(ChatEvent.READY, on_ready)
-    for command in config["General"]["Commands"]:
-        chat.register_command(command["Name"], roll_command(command))
+    for name, command in config["Commands"].items():
+        chat.register_command(name, roll_command(command))
 
     chat.start()
 
@@ -191,16 +244,29 @@ async def run():
 
 yaml = YAML()
 if os.path.exists("config.toml"):
+    print("Upgrading configuraton...")
     with open("config.toml", "rb") as f:
         config = tomllib.load(f)
+
+    config["Commands"] = {x["Name"]: x for x in config["General"]["Commands"]}
+    for x in config["Commands"].values():
+        del x["Name"]
+
+    del config["General"]["Commands"]
+    config["Display"] = config["General"]
+    del config["General"]
 
     with open("config.yml", "w") as f:
         yaml.dump(config, f)
 
+    os.unlink("config.toml")
+
+print("Reading configuraton...")
 with open("config.yml", "r") as f:
     config = yaml.load(f)
 
 
+print("Connecting...")
 cl = obs.ReqClient(
     host=config["OBS"]["Host"],
     port=config["OBS"]["Port"],
